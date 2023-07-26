0ad117f612618abd4c101fde440fa8453596854d
Bereket Zelalem <64479153+bereket-09@users.noreply.github.com>
2023-07-26 15:23:35 +0300

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """
+<<<<<<< HEAD
 A Script that, uses this REST API, for a given employee ID, returns
 information about his/her TODO list progress
 """
 
 import json
+=======
+Using https://jsonplaceholder.typicode.com
+returns info about employee TODO progress
+Implemented using recursion
+"""
+import re
+>>>>>>> parent of d59d8ae... Update 0-gather_data_from_an_API.py
 import requests
 from sys import argv
 
 
+<<<<<<< HEAD
 if __name__ == "__main__":
 
     sessionReq = requests.Session()
@@ -35,3 +44,28 @@ if __name__ == "__main__":
     for done_tasks in json_req:
         if done_tasks['completed']:
             print("\t " + done_tasks.get('title'))
+=======
+
+API = "https://jsonplaceholder.typicode.com"
+"""REST API url"""
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API, id)).json()
+            todos_res = requests.get('{}/todos'.format(API)).json()
+            user_name = user_res.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
+>>>>>>> parent of d59d8ae... Update 0-gather_data_from_an_API.py
